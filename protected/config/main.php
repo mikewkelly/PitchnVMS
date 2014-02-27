0192d7ecd8d658aa2b66b2f713709730bcd3631b
Yudha Syarifudin <yudha@sunblogs.com>
2014-02-27 12:26:57 -0800

    latest update

--- protected/config/main.php
+++ protected/config/main.php
@@ -4,7 +4,6 @@
 // Yii::setPathOfAlias('local','path/to/local-folder');
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
-
 Yii::setPathOfAlias('bootstrap', dirname(__FILE__) . '/../extensions/bootstrap');
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
@@ -18,27 +17,22 @@ return array(
     ),
     'modules' => array(
         // uncomment the following to enable the Gii tool
-        /*
-          'gii' => array(
-          'class' => 'system.gii.GiiModule',
-          'password' => false,
-          // If removed, Gii defaults to localhost only. Edit carefully to taste.
-          //'ipFilters' => array('127.0.0.1', '::1'),
-          ), */
+
         'gii' => array(
-            'generatorPaths' => array(
-                'bootstrap.gii',
-            ),
+            'class' => 'system.gii.GiiModule',
+            'password' => false,
+        // If removed, Gii defaults to localhost only. Edit carefully to taste.
+        //'ipFilters' => array('127.0.0.1', '::1'),
         ),
     ),
     // application components
     'components' => array(
+        'bootstrap' => array(
+            'class' => 'bootstrap.components.Bootstrap',
+        ),
         'user' => array(
             // enable cookie-based authentication
             'allowAutoLogin' => true,
-        ),
-        'bootstrap' => array(
-            'class' => 'bootstrap.components.Bootstrap',
         ),
         // uncomment the following to enable URLs in path-format
         /*
@@ -89,5 +83,4 @@ return array(
         // this is used in contact page
         'adminEmail' => 'webmaster@example.com',
     ),
-    'theme' => 'bootstrap', // requires you to copy the theme under your themes directory
 );
